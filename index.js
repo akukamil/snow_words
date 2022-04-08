f0100d41bc74c21632c5591954c8a1eee214d448
akukamil <chamilioner_kid@mail.ru>
2022-04-08 14:47:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3253,8 +3253,8 @@ async function load_resources() {
 	return;*/
 
 
-	//let git_src="https://akukamil.github.io/snow_words"
-	let git_src=""
+	let git_src="https://akukamil.github.io/snow_words"
+	//let git_src=""
 
 
 	game_res=new PIXI.Loader();
